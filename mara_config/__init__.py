a50511ac7a7f9ed64a244bba0fcff8091643bc3e
Jan Katins <jan.katins@kfzteile24.de>
2018-07-20 17:26:37 +0200

    Remove the config view (moved to mara_app)

--- mara_config/__init__.py
+++ mara_config/__init__.py
@@ -5,21 +5,6 @@ from .app_composing import (call_app_composing_function, init_mara_config_once,
                             register_functionality_in_all_imported_modules)
 
 
-# Contributing the config stuff to the FLASK APP (mara_app)
-def MARA_FLASK_BLUEPRINTS():
-    import mara_config.config_system.view as view
-    yield view.mara_config
-
-
-def MARA_ACL_RESOURCES():
-    import mara_config.config_system.view as view
-    yield view.acl_resource
-
-
-def MARA_NAVIGATION_ENTRY_FNS():
-    import mara_config.config_system.view as view
-    yield view.navigation_entry
-
 # And something to the config system itself
 def MARA_CONFIG_MODULES():
     import mara_config.config
